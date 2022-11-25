706d0e711a15bc326bae3e1db20b5962e8622c55
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2022-11-25 20:54:00 +0545

    update

--- patterns.cpp
+++ patterns.cpp
@@ -179,8 +179,8 @@ using namespace std;
 
 int main(){ 
  for (int i=1; i<=5; i++){
-    for (int j=1; j<=2*i-1; j++){
-        cout<<char(65+j-1);
+    for (int j=1; j<=i; j++){
+        cout<<char(65+i-1);
     }
     cout<<endl;
 }
